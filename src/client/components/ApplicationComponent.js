0fe452ae06ff82943b98fb218c34a910adc34546
jaredscheib <jared.scheib@gmail.com>
2017-02-04 18:06:20 -0800

    Create initial name and comment inputs in app

--- src/client/components/ApplicationComponent.js
+++ src/client/components/ApplicationComponent.js
@@ -1,6 +1,49 @@
-import React from 'react';
-export default ApplicationComponent;
+import React, { Component } from 'react';
 
-function ApplicationComponent(){
-    return <div>Im a the default component. Make me awesome.</div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'Jared',
+      comment: '',
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCommentChange = this.handleCommentChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    const name = e.target.value;
+    this.setState({ name }, () => console.log(this.state));
+  }
+
+  handleCommentChange(e) {
+    const comment = e.target.value;
+    this.setState({ comment }, () => console.log(this.state));
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+        <br />
+        <label htmlFor="comment">Comment: </label>
+        <input
+          type="text"
+          name="comment"
+          value={this.state.comment}
+          onChange={this.handleCommentChange}
+        />
+      </div>
+    );
+  }
 }
+
+export default App;
